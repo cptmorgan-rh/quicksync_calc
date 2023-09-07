26b49f038104111003bba550e6a409aa57e7f76e
Alex Kretzschmar <2773080+ironicbadger@users.noreply.github.com>
2023-09-07 12:42:26 -0400

    Update quicksync-benchmark.sh
    if an existing jf container exists, the name likely clashes.

--- quicksync-benchmark.sh
+++ quicksync-benchmark.sh
@@ -39,7 +39,7 @@ start_container(){
 
   if ! $(docker inspect jellyfin >/dev/null 2>&1); then
     docker pull jellyfin/jellyfin >/dev/null
-    docker run --rm -it -d --name jellyfin --device=/dev/dri:/dev/dri -v $(pwd):/config jellyfin/jellyfin >/dev/null
+    docker run --rm -it -d --name jellyfin-qsvtest --device=/dev/dri:/dev/dri -v $(pwd):/config jellyfin/jellyfin >/dev/null
   fi
 
   sleep 5s
